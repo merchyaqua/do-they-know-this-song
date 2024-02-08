7545e277069d9f04326965a2e23f7f261bc94cf4
merchyaqua <62640172+merchyaqua@users.noreply.github.com>
2024-02-08 16:41:25 +0000

    remove api secrets

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import pylast
 def setup():
-    API_KEY = "d0fdedba80f404a92cf3bb7dcda39230"
-    API_SECRET = "9a15aac73a7776dcf8deee14d7a18f30"
+    API_KEY = ""
+    API_SECRET = ""
+    # when I host this you'll see
     network = pylast.LastFMNetwork(
         api_key=API_KEY,
         api_secret=API_SECRET,
